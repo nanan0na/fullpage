6e181af5a729a8f179bf85866f6dd3c0c425c364
nana <goldbarrr5@gmail.com>
2023-12-21 10:59:09 +0900

    인디케이터 커스텀 설정

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 $(function () {
   // fullpage
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    // 1. 앵커 설정
+    anchors: ['sec1', 'sec2', 'sec3', 'sec4'],
+    // navigation: true,
+
+    // 2. menu적용(인디케이터 커스텀)
+    menu: '#myMenu',
+  });
 });
